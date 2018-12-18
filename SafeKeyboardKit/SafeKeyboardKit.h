b4f546d48c10d39ba821100bf559a13af3f61f48
bamq <you@example.com>
2018-12-18 13:31:54 +0800

    适配carthage

---
+++ SafeKeyboardKit/SafeKeyboardKit.h
@@ -0,0 +1,24 @@
+//
+//  SafeKeyboardKit.h
+//  SafeKeyboardKit
+//
+//  Created by bamq on 2018/12/18.
+//  Copyright © 2018 Aresoft (Shanghai) Tech Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SafeKeyboardKit.
+FOUNDATION_EXPORT double SafeKeyboardKitVersionNumber;
+
+//! Project version string for SafeKeyboardKit.
+FOUNDATION_EXPORT const unsigned char SafeKeyboardKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SafeKeyboardKit/PublicHeader.h>
+
+#import <SafeKeyboardKit/CustomKeyboard.h>
+#import <SafeKeyboardKit/CustomInputAccessoryView.h>
+#import <SafeKeyboardKit/CustomKeyboardView.h>
+#import <SafeKeyboardKit/UITextField+Security.h>
+#import <SafeKeyboardKit/CGSafeTextField.h>
+
